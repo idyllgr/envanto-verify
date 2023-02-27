c94ee532919e2f8b5810471c0d1dc75e55ca2f10
bzn <117572464+idyllgr@users.noreply.github.com>
2023-02-27 14:03:27 +0200

    Update VerifyPurchace.php

--- my_plugin/VerifyPurchace.php
+++ my_plugin/VerifyPurchace.php
@@ -1,45 +1,20 @@
 <?php
-class VerifyPurchace {
-  private $purchase_api_url = 'https://example.com/verify-purchase.php';
+class My_Plugin {
+  private $api_endpoint = 'https://example.com/verify-purchase.php';
 
   public function verify_purchase($purchase_code) {
-    $response = wp_remote_get($this->purchase_api_url . '?purchase_code=' . $purchase_code);
+    $response = wp_remote_get($this->api_endpoint . '?purchase_code=' . urlencode($purchase_code));
     if (is_wp_error($response)) {
-      return array('error' => $response->get_error_message());
+      return new WP_Error('http_error', 'HTTP error: ' . $response->get_error_message());
     }
 
-    $data = json_decode($response['body'], true);
+    $body = wp_remote_retrieve_body($response);
+    $data = json_decode($body, true);
+
     if (isset($data['error'])) {
-      return array('error' => $data['error']);
+      return new WP_Error('verification_error', $data['error']);
     }
 
     return $data;
   }
-
-  private function get_access_token() {
-    return getenv('BZN_FILEPOND_API_ACCESS_TOKEN');
-  }
-
-  public function is_valid_purchase($purchase_code) {
-    $access_token = $this->get_access_token();
-    if (empty($access_token)) {
-      return false;
-    }
-
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://api.envato.com/v3/market/author/sale?code=" . urlencode($purchase_code),
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_HTTPHEADER => array(
-        "Authorization: Bearer $access_token",
-        "User-Agent: bzn-filepond-api/1.0",
-      ),
-    ));
-    $response = curl_exec($curl);
-    $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    curl_close($curl);
-
-    return $http_status === 200;
-  }
 }
-?>
